654d8826389ed19737079629f86a2ab5a35d46f1
bunny-k28 <armandevilk282004@gmail.com>
2022-10-06 00:14:31 +0530

    fixed flash_cmd function

--- __init__.py
+++ __init__.py
@@ -1,9 +1,12 @@
 import os
+import dotenv
 
 def cls():
     os.system('cls')
 
 
 def flash_cmd(command: str, author: str):
+    prefix = dotenv.get_key('Database/SECRETS.env', 'PREFIX')
+
     if author == 'Albus#2627': pass
-    elif ((author != 'Albus#2627') and (command.startswith('.'))): print(f"Command used: {command}\nBy: {author}\n")
+    elif ((author != 'Albus#2627') and (command.startswith(prefix))): print(f"Command used: {command}\nBy: {author}\n")
